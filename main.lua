bdf83367550d29193f4af5e78e16295daf7c5972
turnzack <zacktunr@gmail.com>
2025-01-08 16:49:46 +0100

    main.lua

---
+++ main.lua
@@ -0,0 +1,15 @@
+-- main.lua
+require("login")
+
+function love.load()
+    -- Initialisation du jeu
+    print("Jeu chargé")
+end
+
+function love.update(dt)
+    -- Mise à jour du jeu
+end
+
+function love.draw()
+    -- Dessiner la scène de jeu
+end
