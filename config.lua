07e281578ebae38923530346aca6794c33e36c2f
turnzack <zacktunr@gmail.com>
2025-01-15 23:42:41 +0100

    Delete config.lua

--- config.lua
+++
@@ -1,6 +0,0 @@
--- config.lua
-return {
-    github_client_id = "VOTRE_CLIENT_ID",
-    github_client_secret = "VOTRE_CLIENT_SECRET",
-    encryption_key = "VOTRE_CLEF_DE_CHIFFREMENT" -- Assurez-vous que cela est une clef de 32 caractères pour AES-256
-}
